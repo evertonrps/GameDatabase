b44bf1f4f5c383b904985c92a69a63c9c7922e87
Everton Rocha <evertonrps@gmail.com>
2023-08-20 20:51:57 -0300

    Errors GraphiQL

--- GameDatabase.API/Schema/Mutations/DeveloperMutation.cs
+++ GameDatabase.API/Schema/Mutations/DeveloperMutation.cs
@@ -14,12 +14,19 @@ public class DeveloperMutation
         _developerService = developerService;
     }
     
+    [Error(typeof(MyCustomErrorA))]
+    [Error(typeof(MyCustomErrorB))]
     public async Task<MutationResult<Developer>> CreateDeveloper(string name, DateTime founded, string site)
     {
         var errors = new List<object>();
         if (founded > DateTime.Today)
         {
-            errors.Add(new InvaliMydDateError(founded.ToString()));
+            errors.Add(new MyCustomErrorA("Data inválida"));
+        }
+
+        if (name == "string")
+        {
+            errors.Add(new MyCustomErrorB("Name invalido"));
         }
 
         if (errors.Count > 0)
@@ -30,12 +37,73 @@ public class DeveloperMutation
     }
 }
 
-
-public record InvaliMydDateError
+public class DomainExceptionA
 {
-    public string Message { get; set; }
-    public InvaliMydDateError(string date)
+    public string Message { get;}
+
+    public DomainExceptionA(string message)
     {
-        Message = $"Invalid date time {date}";
+        Message = message; 
     }
+}
+
+public class DomainExceptionB
+{
+    public string Message { get;}
+
+    public DomainExceptionB(string message)
+    {
+        Message = message; 
+    }
+}
+
+public class UserNameTakenError
+{
+}
+
+public class CreateUserErrorFactory
+    : IPayloadErrorFactory<MyCustomError, DomainExceptionA>
+        , IPayloadErrorFactory<MyCustomError, DomainExceptionB>
+{
+    public MyCustomError CreateErrorFrom(DomainExceptionA ex)
+    {
+        return new MyCustomErrorA(ex.Message);
+    }
+
+    public MyCustomError CreateErrorFrom(DomainExceptionB ex)
+    {
+        return new MyCustomErrorB(ex.Message);
+    }
+
+    DomainExceptionA IPayloadErrorFactory<MyCustomError, DomainExceptionA>.CreateErrorFrom(MyCustomError exception)
+    {
+        return new DomainExceptionA(exception.Message);
+    }
+
+    DomainExceptionB IPayloadErrorFactory<MyCustomError, DomainExceptionB>.CreateErrorFrom(MyCustomError exception)
+    {
+        return new DomainExceptionB(exception.Message);
+    }
+}
+
+public class MyCustomErrorB : MyCustomError
+{
+    public MyCustomErrorB(string message)
+    {
+        Message = message;
+    }
+    public string Message { get; set; }
+}
+
+public class MyCustomErrorA : MyCustomError
+{
+    public MyCustomErrorA(string message)
+    {
+        Message = message;
+    }
+    public string Message { get; set; }
+}
+
+public class MyCustomError : Exception
+{
 }
